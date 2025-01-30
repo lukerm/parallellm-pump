5334aae8711a428c71b4ef4923a8a2f40ee121c0
lukerm <13883308+lukerm@users.noreply.github.com>
2025-01-30 20:29:27 +0000

    add table showing head-to-head results for each prompt

--- bin/tally_preferences.py
+++ bin/tally_preferences.py
@@ -5,6 +5,8 @@ from collections import Counter
 from pathlib import Path
 from typing import Dict
 
+import pandas as pd
+
 
 PROVIDERS = ["chatgpt", "claude"]  # Must be in correct order (as originally supplied to pump/prefer.py)
 
@@ -23,9 +25,14 @@ if __name__ == "__main__":
     response_files = os.listdir(response_path)
 
     providers = PROVIDERS
+    rmap = {str(i + 1): provider for i, provider in enumerate(providers)}
     counters = {provider: Counter() for provider in providers}
 
+    rows = []
     for response_file in sorted(response_files):
+
+        prompt_no = response_file.split(".")[0].replace("prompt", "")
+        row = {'prompt_no': prompt_no}
 
         with open(Path(response_path) / response_file, 'r') as f:
             log = f.read()
@@ -40,6 +47,12 @@ if __name__ == "__main__":
             search_pattern += "1. Response ([0-9])"
             grep = re.search(search_pattern, log)
             counters[provider].update([grep.group(1)])
+            row[provider] = rmap[grep.group(1)]
 
-    print("Results:")
+        row['disagree'] = '*' if row[providers[0]] != row[providers[1]] else ''
+        rows.append(row)
+
+    print("Results (raw):")
+    print(pd.DataFrame(rows).to_markdown(index=False))
+    print("\nResults (tallied):")
     print(format_output(counters=counters, rmap={i+1: provider for i, provider in enumerate(providers)}))
