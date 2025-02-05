dc089af1f5584f65d327fdee69ae08a130b9a467
lukerm <13883308+lukerm@users.noreply.github.com>
2025-02-05 10:39:48 +0000

    use relative imports

--- src/config/prompt_config.py
+++ src/config/prompt_config.py
@@ -1,8 +1,8 @@
-from src.utils.providers.alibaba import prompt_alibaba
-from src.utils.providers.anthropic import prompt_anthropic
-from src.utils.providers.deepseek import prompt_deepseek
-from src.utils.providers.google import prompt_google
-from src.utils.providers.openai import prompt_openai
+from ..utils.providers.alibaba import prompt_alibaba
+from ..utils.providers.anthropic import prompt_anthropic
+from ..utils.providers.deepseek import prompt_deepseek
+from ..utils.providers.google import prompt_google
+from ..utils.providers.openai import prompt_openai
 
 PROMPT_FUNCTION_MAP = {
     "alibaba": prompt_alibaba,
